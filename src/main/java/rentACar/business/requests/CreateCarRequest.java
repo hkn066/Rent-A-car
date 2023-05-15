f9b8d7d2f215fa871a816ba6265c62b42666ad02
Hakan Çakır <hakan.cakir@eb.com.tr>
2023-05-15 17:56:08 +0300

    15.05

--- src/main/java/rentACar/business/requests/CreateCarRequest.java
+++ src/main/java/rentACar/business/requests/CreateCarRequest.java
@@ -4,13 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateCarRequest {
+    @NotNull(message = "Plaka Alanı Boş Olamaz.")
+    @NotBlank
     String plate;
+    @NotNull(message = "Model Yılı Boş Olamaz")
+    @NotBlank
     int modelYear;
+//    @NotNull(message = "Model İsmi Boş Olamaz")
+//    @NotBlank
     String modelName;
+
+    String brandName;
+    @NotNull()
+    @NotBlank()
     double dailyPrice;
+
 
 }
